910dfac5f7bca6f2f9372ec7611678ece88b47a4
deadfantom2 <cameratest81@gmail.com>
2019-09-08 23:08:53 +0200

    'first'

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+// Create the User Schema
+const UserSchema = new Schema({
+    name: {type: String, required: true},
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    date: {type: Date, default: Date.now}
+});
+
+// Saves the user's password hashed (plain text password storage is not good)
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+
+// Create method to compare password input to password saved in database
+UserSchema.methods.comparePassword = function(pw, cb) {
+    bcrypt.compare(pw, this.password, function(err, isMatch) {
+        if (err) {
+
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
+module.exports = User = mongoose.model('users', UserSchema);
